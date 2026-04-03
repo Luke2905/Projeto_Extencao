de3c518b82e54cfcc2f799c241267c7f2963934b
Luke2905 <laranjeiralucas17@gmail.com>
2026-04-02 22:20:14 -0300

    Alteração do Select e Adição da visualização dos dados em tabela JTable

--- src/projetoextensao/DataBase.java
+++ src/projetoextensao/DataBase.java
@@ -3,6 +3,9 @@ package projetoextensao;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel; // → Biblioteca de tabela
 
 public class DataBase {
 
@@ -11,9 +14,15 @@ public class DataBase {
 	
 	
 	public void listarDados() throws SQLException {
+		
+		// Definição das colunas da tabela
+		String[] colunas = {"Id", "Id Represa", "Data", "Chuva (mm)", "% Volume", " % Chuva Acumulada - Mês"};
+		
+		//Chamada da classe do modelo para tabela
+		DefaultTableModel modelo = new DefaultTableModel(colunas, 0);
 	
 		//Script SQL
-		String sql = "SELECT * FROM PRODUTOS;";
+		String sql = "SELECT * FROM MONITORAMENTO;";
 		
 		// Metodos de conexao
 		var consulta = con.getConect().prepareStatement(sql);
@@ -21,18 +30,36 @@ public class DataBase {
 		// Execução do Script
 		var resultado = consulta.executeQuery();
 		
-		String aux = "\nLista de Produtos\n\n";
+		//String aux = "\nLista de Produtos\n\n";
 		// resultado.next() pula para a próxima e retorna 'true' se ela existir.
 	    while (resultado.next()) {
+	    	
+	    	// Objeto para a composição de linhas da tablea
+	    	Object[] linha = {
+	    			resultado.getInt("id"),
+	    			resultado.getInt("id_represa"),
+	    			resultado.getDate("data"),
+	    			resultado.getDouble("chuva_mm"),
+	    			resultado.getDouble("volume_util_percent"),
+	    			resultado.getDouble("chuva_acumulada_mes_mm")
+	    	};
+	    	
+	    	// Adiciona linha na tabela
+	    	modelo.addRow(linha);
+	    	/*
 	        String n = resultado.getString("nome"); // Nome
 	        String d = resultado.getString("descricao");
-	        
+	       
 	        aux += "Produto encontrado: " + n + " " + d + "\n";
-	        
+	       */ 
 	        //System.out.println("Produto encontrado: " + n + " " + d);
 	    }
 	    
-	    JOptionPane.showMessageDialog(null, aux);
+	    // chamada da classe java para tabela e Scroll
+	    JTable tabela = new JTable(modelo);
+	    JScrollPane scroll = new JScrollPane(tabela);
+	    
+	    JOptionPane.showMessageDialog(null, scroll);
 	}
 	
 }
